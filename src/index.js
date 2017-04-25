30ba528e59fab1f3c6024af69a785a72e0cd8ce2
Sam Zagrobelny <strangesast@gmail.com>
2017-04-25 13:58:37 -0400

    abc

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { shrinkCropPhoto } from '../src/parse';
 import { setupBlobCommandStream, breakify, streamObjectsFromBlob, formatPercentage } from '../src/stream';
 import * as d3 from 'd3';
 
-let MyWorker = require('./worker.js');
+let MyWorker = require('worker-loader!./worker');
 let worker = new MyWorker();
 
 let eachBlobCommands = setupBlobCommandStream(worker, false);
